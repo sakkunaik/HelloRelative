82a4f160bee88afd5485297a9737ea23ddbfb957
sakkunaik <sakkunaik000@gmail.com>
2018-07-18 12:19:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/hellorelative/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.android.hellorelative;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private	int	mCount	=	0;
+    private TextView mShowCount;
+    String str="The count is:";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.textView2);
+        }
+
+    public void showToast(View view)
+    {
+       // Toast	toast=Toast.makeText(this,	R.string.toast_massage,	Toast.LENGTH_LONG);
+       // toast.show();
+
+        @SuppressLint("ResourceType") Toast	toast1=Toast.makeText(this,	str+mCount,	Toast.LENGTH_LONG);
+        toast1.show();
+
+
+    }
+
+
+    public void countUp(View view)
+    {
+
+        mCount++;
+        if	(mShowCount	!=	null)
+            mShowCount.setText(Integer.toString(mCount));
+
+
+    }
+}
